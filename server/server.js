763fc1913d806b5e8dbc4b9862d0647af555d24e
SLENDERX900 <140382609+SLENDERX900@users.noreply.github.com>
2026-04-15 17:38:34 +0800

    Add Authorization to CORS allowed headers

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5370
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
   if (req.method === 'OPTIONS') return res.sendStatus(200)
   next()
 })
